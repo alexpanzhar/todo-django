9733915ddfa88e5bd508ff15661e28a2484beb1b
alexpanzhar <alex.panzhar1@gmail.com>
2024-08-26 18:08:11 +0200

    feat: create task list page
    The task_list.html was created, the TaskListView was created in registered in todo/url.py

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
+from todo.models import Task
+
 
 # Create your views here.
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
