f6b0ba6320fbc2b27ba588122c29c930d2c268fa
alexpanzhar <alex.panzhar1@gmail.com>
2024-08-26 18:37:34 +0200

    feat: CRUD for Task model was realised
    The views TaskCreate and TaskUpdateView with ./todo/task_form.html template were created. The view TaskDeleteView with ./todo/task_confirm_delete.html was created. All views were registered in ./todo/urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from todo.views import TaskListView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 app_name = "todo"
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task
 
 
@@ -9,3 +10,25 @@ from todo.models import Task
 class TaskListView(generic.ListView):
     model = Task
     paginate_by = 5
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    template_name = "todo/task_form.html"
+
+    form_class = TaskForm
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    template_name = "todo/task_form.html"
+
+    form_class = TaskForm
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    template_name = "todo/task_confirm_delete.html"
