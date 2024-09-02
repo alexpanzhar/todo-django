17239450140cd624ca5051182800955991b3ff43
alexpanzhar <alex.panzhar1@gmail.com>
2024-09-02 14:48:00 +0200

    feat: realise CRUD for tha Tag model
    Views TagListView, TagCreateView, TagUpdateView, TagDeleteView with todo/tag_list.html, todo/tag_form.html, todo/tag_confirm_delete.html
    were created and registered in todo/urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,10 @@ from todo.views import (
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 app_name = "todo"
@@ -14,4 +18,8 @@ urlpatterns = [
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.forms import TaskForm
-from todo.models import Task
+from todo.models import Task, Tag
 
 
 # Create your views here.
@@ -32,3 +32,28 @@ class TaskDeleteView(generic.DeleteView):
     model = Task
     success_url = reverse_lazy("todo:task-list")
     template_name = "todo/task_confirm_delete.html"
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "todo/tag_form.html"
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
+    template_name = "todo/tag_confirm_delete.html"
